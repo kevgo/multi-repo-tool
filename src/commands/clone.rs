50799ce47c10590ac767fead2ab48c2db93493df
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-24 18:20:12 -0500

    clone.rs

--- src/commands/clone.rs
+++ src/commands/clone.rs
@@ -13,16 +13,21 @@ struct GithubRepo {
 pub fn clone(org: &str) {
     println!("Cloning... {}", org);
     // get repos
-    print!("fetch repos ...");
-    let url = format!("https://api.github.com/orgs/{}/repos", org);
     let client = reqwest::blocking::Client::builder()
         .user_agent(APP_USER_AGENT)
         .build()
         .unwrap();
-    let response = client.get(url).send().unwrap();
-    println!(" ok");
-    let headers = next_page_url(response.headers());
-    let repos = response.json::<Vec<GithubRepo>>().unwrap();
+    let mut repos: Vec<GithubRepo> = vec![];
+    let mut url = Some(format!("https://api.github.com/orgs/{}/repos", org));
+    while url.is_some() {
+        print!("fetch {:?} ...", &url);
+        let response = client.get(&url.unwrap()).send().unwrap();
+        println!(" ok");
+        url = next_page_url(response.headers());
+        let parsed = response.json::<Vec<GithubRepo>>().unwrap();
+        repos.extend(parsed);
+    }
+
     // println!("{:#?}", resp);
 
     // clone each repo
@@ -32,32 +37,40 @@ pub fn clone(org: &str) {
     }
 }
 
-fn next_page_url(headers: &HeaderMap) -> String {
+fn next_page_url(headers: &HeaderMap) -> Option<String> {
     for (name, value) in headers {
         if name != "link" {
             continue;
         }
-        let s = value.to_str().unwrap();
-        println!("value: {}", s);
+        return extract_next_link(value.to_str().unwrap());
     }
-    String::new()
+    None
 }
 
-fn extract_next_link(value: &str) -> String {
+fn extract_next_link(value: &str) -> Option<String> {
     let re = Regex::new(r#"<([^>]+)>; rel="next""#).unwrap();
-    let captures = re.captures(value).unwrap();
-    let next = &captures[1];
-    next.to_string()
+    match re.captures(value) {
+        None => return None,
+        Some(captures) => match captures.len() {
+            0 => return None,
+            1 => return None,
+            2 => return Some(captures[1].to_string()),
+            _ => panic!(""),
+        },
+    }
 }
 
 #[cfg(test)]
 mod tests {
 
-    #[test]
-    fn extract_next_link() {
-        let give = r#"<https://api.github.com/organizations/108299804/repos?page=2>; rel="next", <https://api.github.com/organizations/108299804/repos?page=3>; rel="last""#;
-        let want = "https://api.github.com/organizations/108299804/repos?page=2";
-        let have = super::extract_next_link(give);
-        assert_eq!(have, want)
+    mod extract_next_link {
+
+        #[test]
+        fn has_next_link() {
+            let give = r#"<https://api.github.com/organizations/108299804/repos?page=2>; rel="next", <https://api.github.com/organizations/108299804/repos?page=3>; rel="last""#;
+            let want = "https://api.github.com/organizations/108299804/repos?page=2";
+            let have = super::extract_next_link(give);
+            assert_eq!(have, want)
+        }
     }
 }
