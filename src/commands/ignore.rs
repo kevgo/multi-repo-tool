a472deffa1b42b7429c136d02f0aecb6a588acf3
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-25 15:30:52 -0500

    ignore.rs

--- src/commands/ignore.rs
+++ src/commands/ignore.rs
@@ -5,7 +5,7 @@ pub fn ignore(previous_steps: Option<Vec<Step>>) -> Vec<Step> {
         Some(steps) => {
             let mut step_iter = steps.into_iter();
             match step_iter.next() {
-                Some(_) => step_iter.collect::<Vec<_>>(),
+                Some(_) => step_iter.collect(),
                 None => vec![],
             }
         }
