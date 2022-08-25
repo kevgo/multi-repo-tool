f08d9879350db57d1b94b304796c3fd68dc5076e
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-25 13:00:47 -0500

    clone.rs

--- src/commands/clone.rs
+++ src/commands/clone.rs
@@ -3,9 +3,8 @@ use crate::runtime;
 use crate::runtime::Step;
 
 pub fn clone(org: &str) -> Vec<Step> {
-    let repos = github::get_repos(org);
-
     let mut result = vec![];
+    let repos = github::get_repos(org);
     for (i, repo) in repos.into_iter().enumerate() {
         result.push(runtime::clone(i, repo.clone_url));
     }
