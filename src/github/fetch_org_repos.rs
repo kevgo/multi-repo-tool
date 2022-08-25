3eb93d825f76772402d626dfc3f2d29b0c26080f
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-25 17:27:16 -0500

    fetch_org_repos.rs mod.rs

--- src/github/fetch_org_repos.rs
+++ src/github/fetch_org_repos.rs
@@ -1,7 +1,7 @@
-use crate::github;
 use once_cell::sync::Lazy;
 use regex::Regex;
 use reqwest::header::HeaderMap;
+use serde::Deserialize;
 use std::io;
 use std::io::Write;
 use std::mem::drop;
@@ -9,14 +9,20 @@ use std::mem::drop;
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 static LINK_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r#"<([^>]+)>; rel="next""#).unwrap());
 
-pub fn get_repos(org: &str) -> Vec<github::Repo> {
+#[derive(Deserialize)]
+pub struct Repo {
+    pub name: String,
+    pub clone_url: String,
+}
+
+pub fn get_repos(org: &str) -> Vec<Repo> {
     print!("fetching Github org {} .", org);
     drop(io::stdout().flush());
     let client = reqwest::blocking::Client::builder()
         .user_agent(APP_USER_AGENT)
         .build()
         .expect("cannot build HTTP client");
-    let mut repos: Vec<github::Repo> = vec![];
+    let mut repos: Vec<Repo> = vec![];
     let mut next_url = Some(format!("https://api.github.com/orgs/{}/repos", org));
     while let Some(url) = next_url {
         let response = client.get(&url).send().expect("HTTP request failed");
@@ -24,7 +30,7 @@ pub fn get_repos(org: &str) -> Vec<github::Repo> {
         drop(io::stdout().flush());
         next_url = next_page_url(response.headers());
         let parsed = response
-            .json::<Vec<github::Repo>>()
+            .json::<Vec<Repo>>()
             .expect("cannot parse Github API response");
         repos.extend(parsed);
     }
--- src/github/mod.rs
+++ src/github/mod.rs
@@ -1,10 +1,3 @@
 mod fetch_org_repos;
 
 pub use fetch_org_repos::get_repos;
-use serde::Deserialize;
-
-#[derive(Deserialize)]
-pub struct Repo {
-    pub name: String,
-    pub clone_url: String,
-}
