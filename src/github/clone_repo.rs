42b7b70819a4165420bb97f02b549437e9808c8c
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-25 14:00:48 -0500

    clone_repo.rs mod.rs

--- src/github/clone_repo.rs
+++
@@ -1,3 +0,0 @@
-use super::GithubRepo;
-
-pub fn clone_repo(repo: &GithubRepo, i: usize, len: usize) {}
--- src/github/mod.rs
+++ src/github/mod.rs
@@ -1,7 +1,5 @@
-mod clone_repo;
 mod fetch_org_repos;
 
-pub use clone_repo::clone_repo;
 pub use fetch_org_repos::get_repos;
 use serde::Deserialize;
 
