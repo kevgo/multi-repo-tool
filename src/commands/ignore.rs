a785c9185a286fdae119a52a87b37938dee2be01
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-25 15:37:47 -0500

    ignore.rs

--- src/commands/ignore.rs
+++ src/commands/ignore.rs
@@ -1,13 +1,12 @@
 use crate::runtime::Step;
+use std::mem;
 
 pub fn ignore(previous_steps: Option<Vec<Step>>) -> Vec<Step> {
     match previous_steps {
         Some(steps) => {
             let mut step_iter = steps.into_iter();
-            match step_iter.next() {
-                Some(_) => step_iter.collect(),
-                None => vec![],
-            }
+            mem::drop(step_iter.next());
+            step_iter.collect()
         }
         None => vec![],
     }
