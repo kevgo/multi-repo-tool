4a7aa368cf6dcdce94ffb4a7cbcfb3dbcb8e396e
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-25 14:22:57 -0500

    Cargo.lock Cargo.toml runner.rs

--- src/runtime/runner.rs
+++ src/runtime/runner.rs
@@ -1,11 +1,12 @@
 use super::Step;
+use colored::Colorize;
 use std::process::Command;
 
 /// executes the given steps, returns the not executed steps in case of an issue
 pub fn run(steps: Vec<Step>) -> Vec<Step> {
     let mut steps_iter = steps.into_iter();
     while let Some(step) = steps_iter.next() {
-        println!("\n\n{}\n", step);
+        println!("\n\n{}\n", step.to_string().bold());
         let mut command = Command::new(step.command);
         command.args(step.args);
         let success = match command.status() {
