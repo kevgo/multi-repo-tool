c27838b9f30c4548bdfb815b17fbba5c463be1d4
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-29 21:55:54 -0500

    dir_file.rs

--- src/runtime/dir_file.rs
+++ src/runtime/dir_file.rs
@@ -4,8 +4,8 @@ use std::fs;
 
 const FILENAME: &str = "mrt.nextdir";
 
-pub fn save(root: Utf8PathBuf, next_dir: &str) -> Result<(), UserError> {
-    let filepath = root.join(FILENAME);
+pub fn save(initial_dir: Utf8PathBuf, next_dir: &str) -> Result<(), UserError> {
+    let filepath = initial_dir.join(FILENAME);
     fs::write(&filepath, next_dir).map_err(|err| UserError::CannotWriteFile {
         filename: filepath.to_string(),
         guidance: err.to_string(),
