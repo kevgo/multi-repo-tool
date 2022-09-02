cf96e5e0a9ebb8e018b46197a64b903aa57fedc6
Kevin Goslar <kevin.goslar@gmail.com>
2022-09-01 20:57:23 -0500

    limit.rs run.rs

--- src/commands/limit.rs
+++ src/commands/limit.rs
@@ -2,6 +2,7 @@ use crate::config::Config;
 use crate::error::UserError;
 use crate::helpers::get_subdirs;
 use camino::Utf8Path;
+use colored::Colorize;
 use std::process::Command;
 
 pub fn limit(cmd: &str, args: &[String], root_dir: &Utf8Path) -> Result<Config, UserError> {
@@ -9,6 +10,7 @@ pub fn limit(cmd: &str, args: &[String], root_dir: &Utf8Path) -> Result<Config, 
     for dir in get_subdirs(root_dir)? {
         let mut command = Command::new(&cmd);
         command.args(args);
+        command.current_dir(&dir);
         if let Ok(status) = command.status() {
             if let Some(exit_code) = status.code() {
                 if exit_code == 0 {
@@ -16,6 +18,16 @@ pub fn limit(cmd: &str, args: &[String], root_dir: &Utf8Path) -> Result<Config, 
                 }
             }
         }
+    }
+    if folders.is_empty() {
+        return Err(UserError::NoFoldersToIterate);
+    }
+    println!(
+        "\n{}",
+        "Execution has been limited to these folders:".bold()
+    );
+    for (i, folder) in folders.iter().enumerate() {
+        println!("{}. {}", i + 1, folder);
     }
     Ok(Config {
         folders: Some(folders),
