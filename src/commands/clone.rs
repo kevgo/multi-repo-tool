37e469bcaddb46927481c4117f87c0c0501345e4
Kevin Goslar <kevin.goslar@gmail.com>
2022-09-04 17:58:59 -0500

    Set up upstream repos when cloning (#21)

--- src/commands/clone.rs
+++ src/commands/clone.rs
@@ -2,8 +2,9 @@ use crate::config::Config;
 use crate::error::UserError;
 use crate::helpers::github;
 use crate::runtime::steps::{self, Step};
+use camino::Utf8Path;
 
-pub fn clone(org: &str, dir: String) -> Result<Config, UserError> {
+pub fn clone(org: &str, dir: &Utf8Path) -> Result<Config, UserError> {
     let mut steps = vec![];
     let repos = github::get_repos(org)?;
     for repo in repos {
@@ -11,10 +12,27 @@ pub fn clone(org: &str, dir: String) -> Result<Config, UserError> {
             cmd: "git".into(),
             args: vec!["clone".into(), repo.ssh_url],
         });
+        if let Some(parent) = repo.parent {
+            steps.push(Step::Chdir {
+                dir: dir.join(repo.name).into_string(),
+            });
+            steps.push(Step::Run {
+                cmd: "git".into(),
+                args: vec![
+                    "remote".into(),
+                    "add".into(),
+                    "upstream".into(),
+                    parent.ssh_url,
+                ],
+            });
+            steps.push(Step::Chdir {
+                dir: dir.to_string(),
+            });
+        }
     }
     Ok(Config {
         steps: steps::numbered(steps),
         folders: None,
-        root_dir: Some(dir),
+        root_dir: Some(dir.to_string()),
     })
 }
--- src/helpers/github.rs
+++ src/helpers/github.rs
@@ -3,10 +3,10 @@ use once_cell::sync::Lazy;
 use regex::Regex;
 use reqwest::header::HeaderMap;
 use reqwest::StatusCode;
+use serde::de::DeserializeOwned;
 use serde::Deserialize;
 use std::io;
 use std::io::Write;
-use std::mem::drop;
 
 static USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 static LINK_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r#"<([^>]+)>; rel="next""#).unwrap());
@@ -14,6 +14,16 @@ static LINK_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r#"<([^>]+)>; rel="next""#
 #[derive(Deserialize)]
 pub struct Repo {
     pub name: String,
+    /// URL for API access
+    pub url: String,
+    /// URL for `git clone`
+    pub ssh_url: String,
+    /// the parent of the fork
+    pub parent: Option<RepoParent>,
+}
+
+#[derive(Deserialize)]
+pub struct RepoParent {
     pub ssh_url: String,
 }
 
@@ -24,43 +34,55 @@ pub struct ErrorMessage {
 }
 
 pub fn get_repos(org: &str) -> Result<Vec<Repo>, UserError> {
-    print!("fetching Github org {} .", org);
-    drop(io::stdout().flush());
-    let client = reqwest::blocking::Client::builder()
-        .user_agent(USER_AGENT)
-        .build()
-        .expect("cannot build HTTP client");
-    let mut repos: Vec<Repo> = vec![];
+    print!("analyzing github.com/{} ", org);
+    print_dot();
+    let client = create_client();
+    let mut org_repos: Vec<Repo> = vec![];
     let mut next_url = Some(format!("https://api.github.com/orgs/{}/repos", org));
     while let Some(url) = next_url {
         let response = client.get(&url).send().expect("HTTP request failed");
-        print!(".");
-        drop(io::stdout().flush());
+        print_dot();
         next_url = next_page_url(response.headers());
-        match response.status() {
-            StatusCode::OK => {
-                let parsed: Vec<Repo> = response.json().expect("cannot parse Github API response");
-                repos.extend(parsed);
-            }
-            StatusCode::FORBIDDEN => {
-                let error: ErrorMessage = response.json().expect("cannot parse Github API error");
-                return Err(UserError::ApiRequestFailed {
-                    url,
-                    error: error.message,
-                    guidance: error.documentation_url,
-                });
-            }
-            code => {
-                return Err(UserError::UnknownApiError {
-                    url,
-                    code: code.as_u16(),
-                    response: response.text().expect("cannot convert API error to text"),
-                })
-            }
-        }
+        org_repos.extend(parse_response::<Vec<Repo>>(response, url)?);
+    }
+    let mut repos: Vec<Repo> = vec![];
+    for org_repo in org_repos {
+        let url = org_repo.url;
+        let response = client.get(&url).send().expect("HTTP request failed");
+        print_dot();
+        repos.push(parse_response::<Repo>(response, url)?);
     }
     println!(" {} repositories found", repos.len());
     Ok(repos)
+}
+
+fn create_client() -> reqwest::blocking::Client {
+    reqwest::blocking::Client::builder()
+        .user_agent(USER_AGENT)
+        .build()
+        .expect("cannot build HTTP client")
+}
+
+fn parse_response<T>(response: reqwest::blocking::Response, url: String) -> Result<T, UserError>
+where
+    T: DeserializeOwned,
+{
+    match response.status() {
+        StatusCode::OK => Ok(response.json().expect("cannot parse Github API response")),
+        StatusCode::FORBIDDEN => {
+            let error: ErrorMessage = response.json().expect("cannot parse Github API error");
+            Err(UserError::ApiRequestFailed {
+                url,
+                error: error.message,
+                guidance: error.documentation_url,
+            })
+        }
+        code => Err(UserError::UnknownApiError {
+            url,
+            code: code.as_u16(),
+            response: response.text().expect("cannot convert API error to text"),
+        }),
+    }
 }
 
 /// provides the URL to the next set of paginated API results
@@ -77,6 +99,11 @@ fn extract_next_link(value: &str) -> Option<String> {
     LINK_RE
         .captures(value)
         .map(|captures| captures[1].to_string())
+}
+
+fn print_dot() {
+    print!(".");
+    io::stdout().flush().expect("cannot flush stdout");
 }
 
 #[cfg(test)]
