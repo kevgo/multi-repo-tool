f0c1c25abef6fb6a7f99dc0b847966e76534cddb
Kevin Goslar <kevin.goslar@gmail.com>
2022-09-04 10:50:23 -0500

    abort.rs

--- src/commands/abort.rs
+++ src/commands/abort.rs
@@ -1,15 +1,22 @@
 use crate::config::Config;
 use crate::error::UserError;
+use colored::Colorize;
 use std::env;
+use std::path::PathBuf;
 
 pub fn abort(config: Config) -> Result<Config, UserError> {
     if config.steps.is_empty() {
         return Err(UserError::NothingToAbort {});
     }
-    println!("aborting {} steps", config.steps.len());
+    let text = format!("aborting {} steps", config.steps.len());
+    println!("{}", text.bold());
     if let Some(dir) = config.root_dir {
-        println!("returning to the {} dir", &dir);
-        env::set_current_dir(dir).expect("cannot cd into the initial directory");
+        let cwd = env::current_dir().expect("cannot determine current directory");
+        if cwd != PathBuf::from(&dir) {
+            let text = format!("returning to {}", &dir);
+            println!("{}\n", text.bold());
+            env::set_current_dir(dir).expect("cannot cd into the initial directory");
+        }
     }
     Ok(Config {
         steps: vec![],
