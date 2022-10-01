c9bac8918b3852dbb682a950b03c6262f0777400
Kevin Goslar <kevin.goslar@gmail.com>
2022-09-30 20:14:56 -0500

    Start option for walk (#30)

--- src/commands/walk.rs
+++ src/commands/walk.rs
@@ -4,15 +4,28 @@ use crate::helpers::get_subdirs;
 use crate::runtime::steps::{self, Step};
 use camino::Utf8Path;
 
-pub fn walk(root_dir: &Utf8Path, config: Config) -> Result<Config, UserError> {
+pub fn walk(
+    root_dir: &Utf8Path,
+    config: Config,
+    start: Option<String>,
+) -> Result<Config, UserError> {
+    let start = start.map(|start| root_dir.join(start));
     let mut steps = vec![];
+    let mut active = start.is_none();
     let dirs = match config.folders.clone() {
         None => get_subdirs(root_dir)?,
         Some(folders) => folders,
     };
     for dir in dirs {
-        steps.push(Step::Chdir { dir });
-        steps.push(Step::Exit);
+        if let Some(start) = &start {
+            if &dir == start {
+                active = true;
+            }
+        }
+        if active {
+            steps.push(Step::Chdir { dir });
+            steps.push(Step::Exit);
+        }
     }
     steps.push(Step::Chdir {
         dir: root_dir.to_string(),
