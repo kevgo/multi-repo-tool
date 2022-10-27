9b9921239e3d37941fd87b8d9f983a6af9df8725
Kevin Goslar <kevin.goslar@gmail.com>
2022-10-27 14:54:02 -0500

    folder_list.rs

--- src/helpers/folder_list.rs
+++ src/helpers/folder_list.rs
@@ -1,10 +1,12 @@
+use std::fmt::Write as _;
+
 pub fn render(folders: &[String]) -> String {
     let mut result = String::new();
     for (i, folder) in folders.iter().enumerate() {
         match folders.len() {
-            10..=99 => result.push_str(&format!("{:02}. {}\n", i + 1, folder)),
-            100..=999 => result.push_str(&format!("{:03}. {}\n", i + 1, folder)),
-            _ => result.push_str(&format!("{}. {}\n", i + 1, folder)),
+            10..=99 => writeln!(result, "{:02}. {}", i + 1, folder).unwrap(),
+            100..=999 => writeln!(result, "{:03}. {}", i + 1, folder).unwrap(),
+            _ => writeln!(result, "{}. {}", i + 1, folder).unwrap(),
         }
     }
     result
