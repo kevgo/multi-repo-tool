4404418340173505f9f9da0389eaf90ad45b10ce
Kevin Goslar <kevin.goslar@gmail.com>
2022-11-07 16:30:06 -0600

    sort

--- src/commands/limit.rs
+++ src/commands/limit.rs
@@ -45,6 +45,7 @@ pub fn only(
             new_folders.push(dir);
         }
     }
+    new_folders.sort();
     println!("\n");
     if new_folders.is_empty() {
         return Err(UserError::NoFoldersToIterate);
@@ -118,6 +119,7 @@ pub fn unfold(
             }
         }
     }
+    new_folders.sort();
     println!("\n");
     if new_folders.is_empty() {
         return Err(UserError::NoFoldersToIterate);
